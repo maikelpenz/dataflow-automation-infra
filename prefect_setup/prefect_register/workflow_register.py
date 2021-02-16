c918fc287706ba10ffef6f370c8470318942be3a
Maikel Penz <maikelpenz@gmail.com>
2021-02-16 21:08:33 +1300

    feat: fix workflow register call

--- prefect_setup/prefect_register/workflow_register.py
+++ prefect_setup/prefect_register/workflow_register.py
@@ -6,10 +6,10 @@ parser.add_argument("--env", type=str, required=False, default=False)
 parser.add_argument("--prefect_register_token_secret_name", type=str, required=False, default=False)
 
 args, unknown = parser.parse_known_args()
-env = args.env
+environment = args.env
 prefect_register_token_secret_name = args.prefect_register_token_secret_name
 
 # Instantiante Workflow Helpers
 workflow_helpers = WorkflowHelpers()
 # Register Workflow
-workflow_helpers.register_workflow(prefect_register_token_secret_name, env)
+workflow_helpers.register_workflow(environment, prefect_register_token_secret_name)
