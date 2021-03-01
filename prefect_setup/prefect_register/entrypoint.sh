cd9a587f748da74300f5c053cb5c5f4a8e4ffd17
Maikel Penz <maikelpenz@gmail.com>
2021-03-01 21:11:37 +1300

    functional test register workflow

--- prefect_setup/prefect_register/entrypoint.sh
+++ prefect_setup/prefect_register/entrypoint.sh
@@ -31,7 +31,9 @@ ls /tmp/
 echo "----"
 
 echo $workflow_path
-mv $workflow_path/ /tmp/$workflow_path
+mkdir /tmp/$workflow_path
+cp -r $workflow_path/ /tmp/$workflow_path
+#mv $workflow_path/ /tmp/$workflow_path
 
 # move to /tmp/
 #mv $workflow_path/ /tmp/$workflow_path
