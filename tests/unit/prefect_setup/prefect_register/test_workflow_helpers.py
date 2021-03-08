acd54360b3e676d31bb17297ea704a17c99e2985
Maikel Penz <maikelpenz@gmail.com>
2021-03-08 19:37:37 +1300

    feat: introduce ecs agent

--- tests/unit/prefect_setup/prefect_register/test_workflow_helpers.py
+++ tests/unit/prefect_setup/prefect_register/test_workflow_helpers.py
@@ -1,6 +1,7 @@
 from prefect_setup.prefect_register.workflow_helpers import WorkflowHelpers
 
 from botocore.exceptions import ClientError
+from prefect.run_configs import ECSRun
 import pytest
 
 FLOW_NAME = "my-ecr-workflow"
@@ -53,7 +54,7 @@ def test_create_workflow_ecr_repository_fail(mocker):
         workflow_helpers.create_workflow_ecr_repository(FLOW_NAME)
 
 
-def test_set_workflow_properties(mocker):
+def test_set_workflow_properties_ecs_fargate_success(mocker):
     """
     Test setting the workflow environment
     """
@@ -62,14 +63,14 @@ def test_set_workflow_properties(mocker):
 
     mocker.patch(
         "prefect_helpers.PrefectHelpers.get_prefect_aws_infrastructure",
-        return_value=(1, 2, 3),
+        return_value=(1, 2, 3, 4, 5),
     )
     mocker.patch("prefect_setup.prefect_register.workflow_helpers.WorkflowHelpers.import_flow")
 
     flow_module, _ = workflow_helpers.set_workflow_properties(environment, execution_environment)
 
-    # assert flow_module.flow.environment.labels == {f"{environment}_dataflow_automation"}
-    # assert flow_module.flow.environment.launch_type == "FARGATE"
+    assert flow_module.flow.run_config.labels == {f"{environment}_dataflow_automation"}
+    assert isinstance(flow_module.flow.run_config, ECSRun)
 
 
 def test_register_workflow(mocker):
